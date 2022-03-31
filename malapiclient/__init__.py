39b547a502b197f891ab7b7b3b52e2c15a737242
Jonathan Lo <jonathanlo411@gmail.com>
2022-03-31 11:54:34 -0700

    setup auth for public endpoints

--- malapiclient/__init__.py
+++ malapiclient/__init__.py
@@ -1,3 +1,7 @@
+import webbrowser
+import secrets
+import requests
+
 class malclient:
     
     def __init__(self, client_id=None, client_secret=None):
@@ -16,3 +20,14 @@ class malclient:
         url = f'https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id={self.cid}&code_challenge={code}'
         webbrowser.open(url)
         response = input("Authorisation Code: ")
+
+    def get_anime(self, anime_id):
+        url = f'https://api.myanimelist.net/v2/anime/{anime_id}?fields=id,title,main_picture,alternative_titles,synopsis,mean,rank,popularity,num_list_users,media_type,status,genres,my_list_status,num_episodes,start_season,source,studios'
+        response = requests.get(url, headers= {
+            'Authorization': 'X-MAL-CLIENT-ID'
+        })
+        response.raise_for_status()
+        anime_stats = response.json()
+        response.close()
+        return anime_stats
+
