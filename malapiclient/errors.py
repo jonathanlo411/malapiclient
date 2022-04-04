9d6c08798845c26c3f24b11d2910ada339047032
Jonathan Lo <jonathanlo411@gmail.com>
2022-04-04 15:06:44 -0700

    Not authed error implemented

--- malapiclient/errors.py
+++ malapiclient/errors.py
@@ -10,5 +10,6 @@ class NoClientSecretError(Error):
 
 class NeedAuthentificationError(Error):
     def __str__(self):
-        return "No Client Secret. You can add your Client Secret to your malclient by using malclient.add_secret()"
+        return "No Token added. You can create a new one following the Oauth2 process or by using malclient.add_token()"
     pass
+
